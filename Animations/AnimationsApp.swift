cda560b064107cc179e6ffa0a28e1aee6bfba5fa
MaksGorobets <gorobets542@gmail.com>
2023-11-06 17:02:33 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Maks Winters on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
